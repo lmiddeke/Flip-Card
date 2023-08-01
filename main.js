f826649ead7783a551ead2787aa320bec7cd71de
Leonie Middeke <lmiddeke1@gmail.com>
2023-07-31 20:14:42 -0400

    added more js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 document.querySelector('#button').addEventListener('click', launch)
 
 function launch() {
-    
+    const cardFront = document.querySelector('#frontForm').value
+    const cardBack = document.querySelector('#backForm').value
 }
+
+document.querySelector('#cardFront').innerText = cardFront
