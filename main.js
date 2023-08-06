8b05cd592c54d01700fe6ff91c0ddd3db8aaa6a4
Leonie Middeke <lmiddeke1@gmail.com>
2023-08-06 11:43:54 -0400

    Got functionality to work

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 document.querySelector('#button').addEventListener('click', launch)
+document.querySelector('#card').addEventListener('click', flip)
+
 
 function launch() {
     const cardFront = document.querySelector('#frontForm').value
     const cardBack = document.querySelector('#backForm').value
+
+    document.querySelector('#cardFront').innerText = cardFront
 }
 
-document.querySelector('#cardFront').innerText = cardFront
+function flip() {
+    card.classList.toggle("flipper")
+}
